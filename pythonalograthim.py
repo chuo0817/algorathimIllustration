7d77393984b1c32baa45d2d91146b3aae39c2d37
曲绰 <quchuo@qianshengqian.com>
2017-06-22 17:50:06 +0800

    写出个奇怪的结果 ，胃疼

--- pythonalograthim.py
+++ pythonalograthim.py
@@ -63,3 +63,13 @@
 #         return 0
 #     return list[0] + sum(list[1:])
 # print sum([4,5,7,8])
+# 快速排序
+def quicksort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivot = array[0]
+        less = [i for i in array[1:] if i <=pivot ]
+        greater = [i for i in array[1:] if i <=pivot ]
+        return quicksort(less) + [pivot] + quicksort(greater)
+print quicksort([10,5,2,3])
